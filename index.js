da5bc0b56193384dda70699269f321d52f2ab93c
Denny Edy Saputra <ncstealer@gmail.com>
2023-05-14 11:59:34 +0700

    fixed bug

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const qrcode = require('qrcode-terminal');
 const cron = require('node-cron');
 const moment = require('moment-timezone');
-const { Client } = require('whatsapp-web.js');
+const { Client /*, LocalAuth */ } = require('whatsapp-web.js');
+
+
 
 const client = new Client({
     puppeteer: {
         args: ['--no-sandbox'],
     },
+    // authStrategy: new LocalAuth()
 });
 
 client.on('authenticated', (session) => {
@@ -24,35 +27,31 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
     try {
-        const dailyTask = async () => {
+        const dailyTask = () => {
+            console.log("task running..")
             const now = new Date();
             const jakartaTime = moment(now).tz('Asia/Jakarta');
             const dayOfWeek = jakartaTime.day();
             if(dayOfWeek === 0 || dayOfWeek == 7){
-                await client.sendMessage('6285777818508@c.us', `Minggu Jangan Lupa Deadline Beib😘`);
-                return !0;
+                client.sendMessage('6285777818508@c.us', `Minggu Jangan Lupa Deadline Beib😘`);
             }else if(dayOfWeek === 1){
-                await client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen PBO\nJangan Lupa Absen PBO\nJangan Lupa Absen PBO\nJangan Lupa Absen PBO\nJangan Lupa Absen PBO`);
-                return !0;
+                client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen PBO\nJangan Lupa Absen PBO\nJangan Lupa Absen PBO\nJangan Lupa Absen PBO\nJangan Lupa Absen PBO`);
             }else if(dayOfWeek === 2){
-                await client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL`);
-                return !0;
+                client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL\nJangan Lupa Absen KWU dan RPL`);
             }else if(dayOfWeek === 3){
-                await client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi`);
-                return !0;
+                client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi\nJangan Lupa Absen Basis Data dan Sistem Operasi`);
             }else if(dayOfWeek === 4){
-                return !0;
             }else if(dayOfWeek === 5){
-                return !0;
             }else if(dayOfWeek === 6){
-                await client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman`);
-                return !0;
+                client.sendMessage('6285777818508@c.us', `Jangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman\nJangan Lupa Absen Bahasa Pemprogaman`);
             };
         };
-
-        cron.schedule('0 18 * * *', dailyTask, {
-            timezone: 'Asia/Jakarta'
-          });
+        if(message.body === '/start'){
+            await client.sendMessage('6285777818508@c.us', 'Schedule Started...')
+            cron.schedule('0 18 * * *', dailyTask, {
+                timezone: 'Asia/Jakarta'
+            });
+        }
     } catch (error) {
         console.log(error)
     }
