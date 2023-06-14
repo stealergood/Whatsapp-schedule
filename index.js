410bd2385b906c2cfe78f89c0a75fbd7cea26983
stealergood <69463448+stealergood@users.noreply.github.com>
2023-06-14 17:41:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Client , LocalAuth   } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     }
 });
 
